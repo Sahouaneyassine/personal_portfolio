51b05c34bf1757719468c16f64722e88489f60f3
Yassine Sahouane <sahouaneyassine1999@gmail.com>
2020-12-27 18:26:20 +0100

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Project
+
+# Create your views here.
+
+def home(request):
+    projects=Project.objects.all()
+    return render(request,'portfolio/home.html', {"projects":projects})
+
+# Create your views here.
